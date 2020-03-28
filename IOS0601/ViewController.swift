17946e69fa16d1fabead9bcca158e035c4e90354
thomas10011 <tanhuiri@126.com>
2020-03-28 10:21:15 +0800

    Initial Commit

---
+++ IOS0601/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS0601
+//
+//  Created by thomas on 2020/3/28.
+//  Copyright © 2020 thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS0601Tests/IOS0601Tests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS0601Tests.swift
+//  IOS0601Tests
+//
+//  Created by thomas on 2020/3/28.
+//  Copyright © 2020 thomas. All rights reserved.
+//
+
+import XCTest
+@testable import IOS0601
+
+class IOS0601Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
